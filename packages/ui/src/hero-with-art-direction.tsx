5c682d21e0c0441ce7a21cf85ed34e0485d7d413
Mohamed Khaled <mokhaledect@gmail.com>
2026-04-24 16:26:18 +0300

    image opt

--- packages/ui/src/hero-with-art-direction.tsx
+++ packages/ui/src/hero-with-art-direction.tsx
@@ -20,47 +20,59 @@ function mergeImgStyle(base: CSSProperties | undefined): CSSProperties {
   };
 }
 
+const DESKTOP_SIZES = "min(52vw, 510px)";
+const TABLET_SIZES = "calc(100vw - 40px)";
+const MOBILE_SIZES = "100vw";
+
 export function HeroWithArtDirection({
   src,
   alt = DEFAULT_ALT,
 }: HeroWithArtDirectionProps) {
-  const common = {
-    alt,
-    sizes: "(min-width: 1024px) 52vw, (min-width: 640px) 60vw, 100vw" as const,
-    src,
-  };
-
   const {
     props: { srcSet: desktopSrcSet },
   } = getImageProps({
-    ...common,
+    src,
+    alt,
     width: 1440,
     height: 600,
     quality: 85,
+    sizes: DESKTOP_SIZES,
   });
 
   const {
     props: { srcSet: tabletSrcSet },
   } = getImageProps({
-    ...common,
+    src,
+    alt,
     width: 960,
     height: 720,
     quality: 85,
+    sizes: TABLET_SIZES,
   });
 
   const {
     props: { style: mobileStyle, ...imgAttrs },
   } = getImageProps({
-    ...common,
+    src,
+    alt,
     width: 750,
     height: 900,
     quality: 75,
+    sizes: MOBILE_SIZES,
   });
 
   return (
     <picture className="absolute inset-0 block h-full w-full">
-      <source media="(min-width: 1024px)" srcSet={desktopSrcSet} />
-      <source media="(min-width: 640px)" srcSet={tabletSrcSet} />
+      <source
+        media="(min-width: 1024px)"
+        srcSet={desktopSrcSet}
+        sizes={DESKTOP_SIZES}
+      />
+      <source
+        media="(min-width: 640px)"
+        srcSet={tabletSrcSet}
+        sizes={TABLET_SIZES}
+      />
       <img
         {...imgAttrs}
         style={mergeImgStyle(mobileStyle)}
